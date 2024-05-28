182f57ee94686c0964d88655aa3a1db742d2fd81
Donald F. Ferguson (Ansys) <83828368+donff2j@users.noreply.github.com>
2024-05-28 14:03:36 -0400

    Updated theme to Piccolo

--- doc/conf.py
+++ doc/conf.py
@@ -25,5 +25,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # html_theme = 'sphinx_rtd_theme'
-html_theme = 'pyramid'
+html_theme = 'alabaster'
 html_static_path = ['_static']
