754ace560ffdd717200748c50b305c1e711afadd
donald.ferguson <donald.ferguson@ansys.com>
2024-06-14 05:34:09 -0400

    Initial commit.

--- doc/conf.py
+++ doc/conf.py
@@ -25,5 +25,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # html_theme = 'sphinx_rtd_theme'
-html_theme = 'traditional'
+# html_theme = 'traditional'
 html_static_path = ['_static']
+html_theme = 'sphinx_material'
